66d2a299a6cb9df50ab71d437b0e365dbd17da9a
ablfaxl <taghavi.abolfazl@outlook.com>
2023-01-11 23:49:11 +0330

    config folder

---
+++ README.md
@@ -0,0 +1,2 @@
+### Backend of this Project ###
+### url:["https://github.com/thewhiterabbit1994/simple-graph-demo"] ###
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+ <>
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores libero pariatur nostrum voluptates id necessitatibus rem animi, reiciendis mollitia optio earum, veritatis quasi ut vel cupiditate at perferendis distinctio. Officiis?</p>
+ </>
+  );
+}
+
+export default App;
